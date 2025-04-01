efb1d790e3803a65bf2ca320bd0f265e9c373dd7
adelinoemiliano <adelinoemilianoa@gmail.com>
2025-04-01 06:09:43 +0100

    criando de perspectivas e ajustando as routes.

--- src/routes/Rotas.jsx
+++ src/routes/Rotas.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home/Home";
+import Pespectiva from "../pages/Pespectiva/Pespectiva";
 
 export default function Rotas() {
     return (
         <BrowserRouter>
-            <Home />
+            <Routes>
+            <Route path="/" element={<Home />}></Route>
+                <Route path="/pespectiva" element={<Pespectiva />}></Route>
+            </Routes>
         </BrowserRouter>
     )
 }
